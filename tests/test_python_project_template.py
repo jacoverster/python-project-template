1171b912042ebfe55450f67fe5ccd97c0c63d880
jacoverster <jverster@csir.co.za>
2022-07-29 12:43:08 +0000

    first commit

--- tests/test_python_project_template.py
+++ tests/test_python_project_template.py
@@ -3,5 +3,12 @@ import numpy as np
 from python_project_template import convert_list_to_array
 
 
-def test_generate_spline_waypoints():
-    
+def test_convert_list_to_array():
+    list_to_convert = [1,2,3,4,5]
+    expected_result = np.array(list_to_convert)
+    list_array = convert_list_to_array(list_to_convert)
+
+    # Assert list values are equal before and after conversion
+    np.testing.assert_equal(list_array, expected_result)
+    # Assert type
+    assert isinstance(list_array, np.ndarray)
