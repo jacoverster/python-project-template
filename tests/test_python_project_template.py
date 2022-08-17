eed0acd1d072c0d31a257fbe505f918620c897dc
jacoverster <jverster@csir.co.za>
2022-08-17 15:01:06 +0000

    updated pre-commit hooks and devcontainer config

--- tests/test_python_project_template.py
+++ tests/test_python_project_template.py
@@ -4,7 +4,7 @@ from python_project_template import convert_list_to_array
 
 
 def test_convert_list_to_array():
-    list_to_convert = [1,2,3,4,5]
+    list_to_convert = [1, 2, 3, 4, 5]
     expected_result = np.array(list_to_convert)
     list_array = convert_list_to_array(list_to_convert)
 
